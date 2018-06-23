c2969c2638b1a9ee795e54b5e9fd2f3d8f1bbc8e
James Illing <James@sherwood37.demon.co.uk>
2018-06-23 19:33:26 +0100

    Added some files from attempt 1

---
+++ PXRF-processor/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using PXRFProcessor;
+
+namespace PXRF_processor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 0)
+            {
+                Console.WriteLine("Missing file name.");
+                return;
+            }
+
+            var file = args[0];
+
+            Console.WriteLine($"Reading {file}");
+            var lines = File.ReadLines(file);
+
+            var records = lines.Select(PxrfRecord.CreateRecord).Where(record => record != null).ToList();
+
+            records.AddRange(records.GroupBy(x => x.Record).Select(g =>
+                new PxrfRecord {Record = g.Key,
+                    RecordTitle = g.Key + "-Avg",
+                    AsKa1 = g.Average(a => a.AsKa1),
+                    BaLa1 = g.Average(a=>a.BaLa1),
+                    CaKa1 = g.Average(a=>a.CaKa1),
+                    CrKa1 = g.Average(a=>a.CrKa1),
+                    CoKa1 = g.Average(a=>a.CoKa1),
+                    CuKa1 = g.Average(a => a.CuKa1),
+                    FeKa1 = g.Average(a => a.FeKa1),
+                    MnKa1 = g.Average(a => a.MnKa1),
+                    MoKa1 = g.Average(a => a.MoKa1),
+                    NbKa1 = g.Average(a => a.NbKa1),
+                    NiKa1 = g.Average(a => a.NiKa1),
+                    PbLa1 = g.Average(a => a.PbLa1),
+                    RbKa1 = g.Average(a=>a.RbKa1),
+                    RhKa1 = g.Average(a => a.RhKa1),
+                    SbKa1 = g.Average(a => a.SbKa1),
+                    SrKa1 = g.Average(a => a.SrKa1),
+                    SnKa1 = g.Average(a => a.SnKa1),
+                    ThLa1 = g.Average(a=> a.ThLa1),
+                    TiKa1 = g.Average(a => a.TiKa1),
+                    ULa1 = g.Average(a=>a.ULa1),
+                    YKa1 = g.Average(a => a.YKa1),
+                    ZnKa1 = g.Average(a => a.ZnKa1),
+                    ZrKa1 = g.Average(a => a.ZrKa1),
+                }));
+
+            File.WriteAllLines("c:\\result\\Output.csv",records.OrderBy(x=>x.RecordTitle).Select(x=>x.ToCsv()));
+        }
+    }
+}
---
+++ PXRF-processor/PxrfRecord.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace PXRFProcessor
+{
+    public class PxrfRecord
+    {
+        private const string Separator = ",";
+
+        public static PxrfRecord CreateRecord(string record)
+        {
+
+            var columns = record.Split(Separator.ToCharArray());
+            if (columns.Length != 24)
+            {
+                return null;
+            }
+            for(var i=0; i<columns.Length;i++)
+            {
+                columns[i] = columns[i].Trim('\"');
+            }
+
+            try
+            {
+                return new PxrfRecord
+                {
+                    RecordTitle = columns[0],
+                    Record = columns[0].Substring(0, columns[0].IndexOf("-", StringComparison.Ordinal)),
+                    CaKa1 = decimal.Parse(columns[1]),
+                    BaLa1 = decimal.Parse(columns[2]),
+                    TiKa1 = decimal.Parse(columns[3]),
+                    CrKa1 = decimal.Parse(columns[4]),
+                    MnKa1 = decimal.Parse(columns[5]),
+                    FeKa1 = decimal.Parse(columns[6]),
+                    CoKa1 = decimal.Parse(columns[7]),
+                    NiKa1 = decimal.Parse(columns[8]),
+                    CuKa1 = decimal.Parse(columns[9]),
+                    ZnKa1 = decimal.Parse(columns[10]),
+                    AsKa1 = decimal.Parse(columns[11]),
+                    PbLa1 = decimal.Parse(columns[12]),
+                    ThLa1 = decimal.Parse(columns[13]),
+                    RbKa1 = decimal.Parse(columns[14]),
+                    ULa1 = decimal.Parse(columns[15]),
+                    SrKa1 = decimal.Parse(columns[16]),
+                    YKa1 = decimal.Parse(columns[17]),
+                    ZrKa1 = decimal.Parse(columns[18]),
+                    NbKa1 = decimal.Parse(columns[19]),
+                    MoKa1 = decimal.Parse(columns[20]),
+                    RhKa1 = decimal.Parse(columns[21]),
+                    SnKa1 = decimal.Parse(columns[22]),
+                    SbKa1 = decimal.Parse(columns[23])
+                };
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+
+        public string ToCsv()
+        {
+            return
+                $"\"{RecordTitle}\",\"{CaKa1}\",\"{BaLa1}\",\"{TiKa1}\",\"{CrKa1}\",\"{MnKa1}\",\"{FeKa1}\",\"{CoKa1}\",\"{NiKa1}\",\"{CuKa1}\",\"{ZnKa1}\",\"{AsKa1}\",\"{PbLa1}\",\"{ThLa1}\",\"{RbKa1}\",\"{ULa1}\",\"{SrKa1}\",\"{YKa1}\",\"{ZrKa1}\",\"{NbKa1}\",\"{MoKa1}\",\"{RhKa1}\",\"{SnKa1}\",\"{SbKa1}\"";
+        }
+
+        public string RecordTitle { get; set; }
+        public string Record { get; set; }        
+        public decimal CaKa1 { get; set; }
+        public decimal BaLa1 { get; set; }
+        public decimal TiKa1 { get; set; }
+        public decimal CrKa1 { get; set; }
+        public decimal MnKa1 { get; set; }
+        public decimal FeKa1 { get; set; }
+        public decimal CoKa1 { get; set; }
+        public decimal NiKa1 { get; set; }
+        public decimal CuKa1 { get; set; }
+        public decimal ZnKa1 { get; set; }
+        public decimal AsKa1 { get; set; }
+        public decimal PbLa1 { get; set; }
+        public decimal ThLa1 { get; set; }
+        public decimal RbKa1 { get; set; }
+        public decimal ULa1 { get; set; }
+        public decimal SrKa1 { get; set; }
+        public decimal YKa1 { get; set; }
+        public decimal ZrKa1 { get; set; }
+        public decimal NbKa1 { get; set; }
+        public decimal MoKa1 { get; set; }
+        public decimal RhKa1 { get; set; }
+        public decimal SnKa1 { get; set; }
+        public decimal SbKa1 { get; set; }
+    }
+}
